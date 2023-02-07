32865ffb80f55eeddbc8877bffb6a1604e89beef
Catherine Volkova <catherine@taxfix.de>
2023-02-07 23:53:34 +0100

    Remove path from deployment Create folder if missing

--- week_2_prefect/etl_web_to_gcs.py
+++ week_2_prefect/etl_web_to_gcs.py
@@ -29,6 +29,7 @@ def clean(df: pd.DataFrame) -> pd.DataFrame:
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out locally as parquet file"""
     path = Path(f"data/{color}/{dataset_file}.parquet")
+    path.parent.mkdir(parents=True, exist_ok=True)
     df.to_parquet(path, compression="gzip")
     return path
 
